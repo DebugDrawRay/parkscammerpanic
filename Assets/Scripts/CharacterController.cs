c68d125d62f234f5b1e6e049ac27d6df792fa063
Raymond John Grice <raymondjgrice@gmail.com>
2017-08-05 00:05:36 -0700

    Movement, character controller, input

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,7 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour 
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterController : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveAccel;
+	public float maxSpeed;
+	private float currentSpeed;
+	private Vector3 currentDirection;
 
+    private Rigidbody rigid;
+    private PlayerActions actions;
+
+    public enum State
+    {
+        Idle,
+        Active
+    }
+    public State currentState;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        currentState = State.Active;
+        actions = PlayerActions.BindAll();
+    }
+
+    private void Update()
+    {
+        RunStates();
+    }
+    private void FixedUpdate()
+    {
+        RunFixedStates();
+    }
+
+    private void RunStates()
+    {
+        switch (currentState)
+        {
+            case State.Idle:
+                break;
+            case State.Active:
+                break;
+        }
+    }
+    private void RunFixedStates()
+    {
+        switch (currentState)
+        {
+            case State.Idle:
+                break;
+            case State.Active:
+                MovementListener();
+                break;
+        }
+    }
+    private void MovementListener()
+    {
+        Vector3 move = actions.Move.Value;
+        move.z = move.y;
+        move.y = 0;
+        if (move == Vector3.zero)
+        {
+            if (currentSpeed > 0)
+            {
+                currentSpeed -= moveAccel;
+                currentSpeed = Mathf.Clamp(currentSpeed, 0, Mathf.Infinity);
+            }
+        }
+        else
+        {
+			currentDirection = move;
+            currentSpeed += moveAccel;
+            currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
+        }
+		Vector3 movement = currentDirection * currentSpeed;
+        rigid.MovePosition(transform.position + (movement * Time.deltaTime));
+    }
 }
