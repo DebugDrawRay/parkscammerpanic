ff56db846dbd391f79e63f4579d92ee9c8d11687
seraphki <sephki@gmail.com>
2017-08-06 14:19:36 -0700

    Color Randomization Update

--- Assets/Scripts/LevelColorRandomizer.cs
+++ Assets/Scripts/LevelColorRandomizer.cs
@@ -23,19 +23,21 @@ public class LevelColorRandomizer : MonoBehaviour
     {
         MeshRenderer[] meshRenderers = (MeshRenderer[])FindObjectsOfType<MeshRenderer>();
         for (int i = 0; i < meshRenderers.Length; i++)
-        {         
-            if (meshRenderers[i].sharedMaterial == WindowsDoorsMaterial)
+        {
+            for (int m = 0; m < meshRenderers[i].sharedMaterials.Length; m++)
             {
-                int colorRandom = Random.Range(0, WindowsDoorsColors.Length);
-                Color color = WindowsDoorsColors[colorRandom];
-                meshRenderers[i].material.SetColor("_EmissionColor", color);
-            }
-
-            if (meshRenderers[i].sharedMaterial == BillboardsMaterial)
-            {
-                int colorRandom = Random.Range(0, BillboardColors.Length);
-                Color color = BillboardColors[colorRandom];
-                meshRenderers[i].material.SetColor("BG_Color", color);
+                if (meshRenderers[i].materials[m].name.Contains(WindowsDoorsMaterial.name))
+                {
+                    int colorRandom = Random.Range(0, WindowsDoorsColors.Length);
+                    Color color = WindowsDoorsColors[colorRandom];
+                    meshRenderers[i].materials[m].SetColor("_EmissionColor", color);
+                }
+                else if (meshRenderers[i].materials[m].name.Contains(BillboardsMaterial.name))
+                {
+                    int colorRandom = Random.Range(0, BillboardColors.Length);
+                    Color color = BillboardColors[colorRandom];
+                    meshRenderers[i].materials[m].SetColor("_BG_Color", color);
+                }
             }
         }
     }
