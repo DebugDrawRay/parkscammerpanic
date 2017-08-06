3d3869dd97d918d0a9a189dad3d532d8e7559e07
seraphki <sephki@gmail.com>
2017-08-06 02:52:02 -0700

    Start Screen Update

--- Assets/Scripts/StartSceneController.cs
+++ Assets/Scripts/StartSceneController.cs
@@ -1,29 +1,80 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class StartSceneController : MonoBehaviour
 {
-    public InputField LevelInput;
-    public int MaxLevel = 1;
+    [Header("Main Screens")]
+    public RectTransform StartScreen;
+    public RectTransform LevelSelectScreen;
 
-    public void StartGame()
+    [Header("Level Select")]
+    public LevelSelection[] LevelSelections;
+    public RectTransform LevelDisplay;
+    public Text LevelNameText;
+    public Image LevelDisplayImage;
+
+    private int _selectedLevel = 0;
+
+    private int _offscreen = 2000;
+    private float _scrollSpeed = 0.6f;
+
+    private void Start()
     {
-        int randomLevel = Random.Range(1, MaxLevel + 1);
-        StartGameAtLevel(randomLevel);
+        UpdateLevelDisplay();
     }
 
-    public void StartGameAtLevelButton()
+    private void StartGame()
     {
-        if (LevelInput.text != null)
-        {
-            int level = System.Convert.ToInt32(LevelInput.text);
-            StartGameAtLevel(level);
-        }
+        SceneManager.LoadScene(LevelSelections[_selectedLevel].SceneName);
     }
 
-    private void StartGameAtLevel(int level)
+    public void ShowLevelSelect()
     {
-        SceneManager.LoadScene("Level" + level.ToString());
+        StartScreen.DOAnchorPos(new Vector3(-_offscreen, 0, 0), _scrollSpeed / 2f).SetEase(Ease.OutBounce);
+        LevelSelectScreen.DOAnchorPos(new Vector3(0, 0, 0), _scrollSpeed).SetEase(Ease.OutBounce);
+    }
+
+    public void NextLevel()
+    {
+        LevelDisplay.DOAnchorPos(new Vector3(-_offscreen, 0, 0), _scrollSpeed/2f).OnComplete(() => CompleteNextLevelMovement()).SetEase(Ease.InCubic);
+    }
+
+    public void CompleteNextLevelMovement()
+    {
+        _selectedLevel = _selectedLevel == LevelSelections.Length - 1 ? 0 : _selectedLevel + 1;
+        UpdateLevelDisplay();
+
+        LevelDisplay.anchoredPosition = new Vector3(_offscreen, 0, 0);
+        LevelDisplay.DOAnchorPos(new Vector3(0, 0, 0), _scrollSpeed).SetEase(Ease.OutBounce);
+    }
+
+    public void PreviousLevel()
+    {
+        LevelDisplay.DOAnchorPos(new Vector3(_offscreen, 0, 0), _scrollSpeed/2f).OnComplete(() => CompletePreviousLevelMovement()).SetEase(Ease.InCubic);
+    }
+
+    public void CompletePreviousLevelMovement()
+    {
+        _selectedLevel = _selectedLevel == 0 ? LevelSelections.Length - 1 : _selectedLevel - 1;
+        UpdateLevelDisplay();
+
+        LevelDisplay.anchoredPosition = new Vector3(-_offscreen, 0, 0);
+        LevelDisplay.DOAnchorPos(new Vector3(0, 0, 0), _scrollSpeed).SetEase(Ease.OutBounce);
+    }
+
+    public void UpdateLevelDisplay()
+    {
+        LevelNameText.text = LevelSelections[_selectedLevel].LevelName;
+        LevelDisplayImage.sprite = LevelSelections[_selectedLevel].LevelImage;
+    }
+
+    [System.Serializable]
+    public class LevelSelection
+    {
+        public Sprite LevelImage;
+        public string LevelName;
+        public string SceneName;
     }
 }
